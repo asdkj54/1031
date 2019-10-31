a33f2726f32dad18432810ef66de5198eec6d980
asdkj54 <1450438214@qq.com>
2019-10-31 20:47:54 +0800

    添加项目文件。

---
+++ 1031/1031View.cpp
@@ -0,0 +1,127 @@
+﻿
+// 1031View.cpp: CMy1031View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "1031.h"
+#endif
+
+#include "1031Doc.h"
+#include "1031View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMy1031View
+
+IMPLEMENT_DYNCREATE(CMy1031View, CView)
+
+BEGIN_MESSAGE_MAP(CMy1031View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CMy1031View::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMy1031View 构造/析构
+
+CMy1031View::CMy1031View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMy1031View::~CMy1031View()
+{
+}
+
+BOOL CMy1031View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMy1031View 绘图
+
+void CMy1031View::OnDraw(CDC* /*pDC*/)
+{
+	CMy1031Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMy1031View 打印
+
+
+void CMy1031View::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CMy1031View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMy1031View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMy1031View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CMy1031View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMy1031View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMy1031View 诊断
+
+#ifdef _DEBUG
+void CMy1031View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMy1031View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMy1031Doc* CMy1031View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMy1031Doc)));
+	return (CMy1031Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMy1031View 消息处理程序
---
+++ 1031/1031View.h
@@ -0,0 +1,52 @@
+﻿
+// 1031View.h: CMy1031View 类的接口
+//
+
+#pragma once
+
+
+class CMy1031View : public CView
+{
+protected: // 仅从序列化创建
+	CMy1031View() noexcept;
+	DECLARE_DYNCREATE(CMy1031View)
+
+// 特性
+public:
+	CMy1031Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMy1031View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // 1031View.cpp 中的调试版本
+inline CMy1031Doc* CMy1031View::GetDocument() const
+   { return reinterpret_cast<CMy1031Doc*>(m_pDocument); }
+#endif
+
